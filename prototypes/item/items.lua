fd50eed07641f66bd1da8ae2e99c92d395826b3b
billbo99 <cactus_computers-github@yahoo.com>
2020-09-01 19:23:01 +0100

    belt and inserters updated now too

--- prototypes/item/items.lua
+++ prototypes/item/items.lua
@@ -21,6 +21,20 @@ local items = {
     {source = "fluid-wagon", name = "fluid-wagon-mk2", subgroup = "fb-vehicle", order = "h[fluid-wagon-mk2]-c", icon_name = "fluid-wagon-mk2.png", itype = "item-with-entity-data"},
     {source = "fluid-wagon", name = "fluid-wagon-mk3", subgroup = "fb-vehicle", order = "h[fluid-wagon-mk3]-d", icon_name = "fluid-wagon-mk3.png", itype = "item-with-entity-data"},
     --
+    {source = "transport-belt", name = "rapid-transport-belt-mk1", subgroup = "fb-transport", order = "b[rapid-transport-belt-mk1]-a", icon_name = "rapid-transport-belt-mk1.png", itype = "item"},
+    {source = "transport-belt", name = "rapid-transport-belt-mk2", subgroup = "fb-transport", order = "b[rapid-transport-belt-mk2]-b", icon_name = "rapid-transport-belt-mk2.png", itype = "item"},
+    {source = "underground-belt", name = "rapid-transport-belt-to-ground-mk1", subgroup = "fb-transport", order = "b[rapid-transport-belt-to-ground-mk1]-a", icon_name = "rapid-underground-belt-mk1.png", itype = "item"},
+    {source = "underground-belt", name = "rapid-transport-belt-to-ground-mk2", subgroup = "fb-transport", order = "b[rapid-transport-belt-to-ground-mk2]-b", icon_name = "rapid-underground-belt-mk2.png", itype = "item"},
+    {source = "splitter", name = "rapid-splitter-mk1", subgroup = "fb-transport", order = "d[rapid-splitter-mk1]-a", icon_name = "rapid-splitter-mk1.png", itype = "item"},
+    {source = "splitter", name = "rapid-splitter-mk2", subgroup = "fb-transport", order = "d[rapid-splitter-mk2]-b", icon_name = "rapid-splitter-mk2.png", itype = "item"},
+    --
+    {source = "inserter", name = "fast-inserter-mk2", subgroup = "fb-transport-inserters", order = "a[fast-inserter-mk2]-a", icon_name = "fast-inserter-mk2.png", itype = "item"},
+    {source = "inserter", name = "fast-inserter-mk3", subgroup = "fb-transport-inserters", order = "a[fast-inserter-mk3]-b", icon_name = "fast-inserter-mk3.png", itype = "item"},
+    {source = "inserter", name = "fast-long-handed-inserter", subgroup = "fb-transport-inserters", order = "b[fast-long-handed-inserter]-a", icon_name = "fast-long-handed-inserter.png", itype = "item"},
+    {source = "inserter", name = "filter-long-handed-inserter", subgroup = "fb-transport-inserters", order = "b[smart-long-handed-inserter]-b", icon_name = "smart-long-handed-inserter.png", itype = "item"},
+    {source = "inserter", name = "stack-inserter-mk2", subgroup = "fb-transport-inserters", order = "c-a", icon_name = "stack-inserter-mk2.png", itype = "item"},
+    {source = "inserter", name = "stack-filter-inserter-mk2", subgroup = "fb-transport-inserters", order = "c-b", icon_name = "stack-filter-inserter-mk2.png", itype = "item"},
+    --
     {source = "pipe", name = "pipe-mk2", subgroup = "fb-fluids", order = "a-a", icon_name = "pipe-mk2.png", itype = "item"},
     {source = "pipe-to-ground", name = "pipe-to-ground-mk2", subgroup = "fb-fluids", order = "a-c", icon_name = "pipe-to-ground-mk2.png", itype = "item"},
     {source = "storage-tank", name = "storage-tank-mk2", subgroup = "fb-fluids", order = "b-a", icon_name = "storage-tank-mk2.png", itype = "item"},
